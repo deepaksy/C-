326413ee426af940ca8cc9cdfc166de114ad15a3
deepaksy <deepaksy28@gmail.com>
2022-07-13 10:43:05 +0530

    initial commit

---
+++ shape.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"         ."<<endl;
+    cout<<"        /|"<<endl;
+    cout<<"       / |"<<endl;
+    cout<<"      /  |"<<endl;
+    cout<<"     /   |"<<endl;
+    cout<<"    /    |"<<endl;
+    cout<<"   /     |"<<endl;
+    cout<<"  /      |"<<endl;
+    cout<<" /       |"<<endl;
+    cout<<"/________|"<<endl;
+    return 0;
+}
---
+++ variables/variable.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include<string.h>
+int main(){
+    char name[]="Deepak";
+    std::cout<<name;
+    return 0;
+}
